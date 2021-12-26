57731d4079084f5c9f4ffc578046099b65cf7557
Fabio Milheiro <fabiomilheiro@gmail.com>
2021-12-26 15:27:19 +0000

    Update BasketItem.cs

--- LittleBasket/BasketItem.cs
+++ LittleBasket/BasketItem.cs
@@ -1,13 +1,3 @@
 ﻿namespace LittleBasket;
 
-public class BasketItem
-{
-    public BasketItem(Product product, int quantity)
-    {
-        this.Product = product;
-        this.Quantity = quantity;
-    }
-
-    public Product Product { get; }
-    public int Quantity { get; set; }
-}
+public record BasketItem(Product Product, int Quantity);
